0e2a03be0af1942441e6d3d1c0610b808a94559f
ronny-antoon <90724911+ronny-antoon@users.noreply.github.com>
2023-11-11 00:58:06 +0200

    Update ButtonModuleInterface.hpp
    add the default value for Abstract class

--- include/ButtonModuleInterface.hpp
+++ include/ButtonModuleInterface.hpp
@@ -68,7 +68,7 @@ public:
      * @param longPressTime The long press time for button triggers.
      * @param timeBetweenDoublePress The time between double presses for button triggers.
      */
-    virtual void startListening(uint32_t usStackDepth, uint8_t checkInterval, uint8_t debounceTime, uint16_t longPressTime, uint16_t timeBetweenDoublePress) = 0;
+    virtual void startListening(uint32_t usStackDepth = 10000, uint8_t checkInterval = 30, uint8_t debounceTime = 90, uint16_t longPressTime = 1000, uint16_t timeBetweenDoublePress = 500) = 0;
 
     /**
      * @brief Stops listening for button triggers.
